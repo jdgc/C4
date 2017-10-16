b28f4f828f0870e717c6959e53f257ca1cad812a
James Dunn <james.ed.dunn@gmail.com>
2017-10-16 17:11:56 +0900

    addd username migration and signup field

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,7 @@
 class Game < ApplicationRecord
 
   CONSOLES = ["Wii U", "PlayStation 4", "Xbox One", "Xbox 360", "PlayStation 3", "Wii", "Dreamcast", "PlayStation 2"]
-  belongs_to :user, prsence: true
+  belongs_to :user
 
   validates :name, presence: true
   validates :description, presence: true
