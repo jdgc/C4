e353a196bb72e2c4bbf5d5923869d2d8fb698e15
James Dunn <james.ed.dunn@gmail.com>
2017-10-16 15:43:00 +0900

    search updates

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,13 +1,13 @@
 class Game < ApplicationRecord
   belongs_to :user
     CONSOLES = ["Wii U", "PlayStation 4", "Xbox One", "Xbox 360", "PlayStation 3", "Wii", "Dreamcast", "PlayStation 2"]
-  validates :title, presence: true
+  validates :name, presence: true
   validates :description, presence: true
   validates :console, inclusion: { in: CONSOLES }
 
 
   def self.search(search)
-  where("title ILIKE ? OR description ILIKE ? OR console ILIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
+  where("name ILIKE ? OR description ILIKE ? OR console ILIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
   end
 
 end
