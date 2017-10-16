f8b647e238acfd8d58f81714b5e6522d5c2f1680
James Dunn <james.ed.dunn@gmail.com>
2017-10-16 13:42:59 +0900

    game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,7 @@
 class Game < ApplicationRecord
   belongs_to :user
+    CONSOLES = ["Wii U", "PlayStation 4", "Xbox One", "Xbox 360", "PlayStation 3", "Wii", "Dreamcast", "PlayStation 2"]
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :console
 end
