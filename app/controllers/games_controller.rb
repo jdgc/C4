e2af7a78d99bdbc370c39c92f659350eeb5570ce
James Dunn <james.ed.dunn@gmail.com>
2017-10-16 15:25:23 +0900

    search update

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,12 @@
 class GamesController < ApplicationController
 
   def index
-    @games = Game.all
+    @games = Game.
+    if params[:search]
+    @games = Game.search(params[:search]).order("created_at DESC")
+  else
+    @games = Game.all.order("created_at DESC")
+  end
   end
 
   def show
