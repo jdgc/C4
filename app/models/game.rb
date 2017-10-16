86092c339ee0bb03fa840567e1a2bbbff934eb6b
James Dunn <james.ed.dunn@gmail.com>
2017-10-16 13:56:41 +0900

    game controller methods and console verify

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,5 +3,6 @@ class Game < ApplicationRecord
     CONSOLES = ["Wii U", "PlayStation 4", "Xbox One", "Xbox 360", "PlayStation 3", "Wii", "Dreamcast", "PlayStation 2"]
   validates :title, presence: true
   validates :description, presence: true
-  validates :console
+  validates :console, inclusion: { in: CONSOLES }
 end
+
