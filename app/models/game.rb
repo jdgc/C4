3f952c2ffd12e0afc7e1de2aa37b851655acc160
trevor malard <redsky.dz@gmail.com>
2017-10-17 19:14:35 +0900

    REFACTORED HOME PAGE BAY BEEE

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,6 @@
 class Game < ApplicationRecord
 
-  CONSOLES = ["PlayStation", "Xbox", "Wii", "Gamecube", "Nintendo 64", "Super Nintendo"]
+  CONSOLES = ["Nintendo 64", "PlayStation", "Super Nintendo", "Xbox", "Wii", "Gamecube"]
   belongs_to :owner, class_name: 'User'
   has_attachment :photo
   validates :name, :owner, :description, presence: true
