324de0983c4d41890b549c3e53863fcbcfcfa443
trevor malard <redsky.dz@gmail.com>
2017-10-17 16:04:31 +0900

    user profile model created

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @games = Game.where(owner_id:@user.id)
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    if @user != current_user
+      # NOT CURRENT USER
+      redirect_to edit_user_path(current_user)
+    end
+  end
+
+  def update
+    @user = current_user
+    @user.update(user_params)
+    @user.save
+    redirect_to user(@user)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :username)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
     resources :rentals, only: [:new, :create]
   end
   resources :rentals, only: [:show]
+  resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
