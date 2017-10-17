c93972e550bf5c1d7c03a40ecdc85c61b43cf519
James Dunn <james.ed.dunn@gmail.com>
2017-10-17 12:36:10 +0900

    image upload implemented - attachinary/cloudinary

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,7 +2,7 @@ class Game < ApplicationRecord
 
   CONSOLES = ["Wii U", "PlayStation 4", "Xbox One", "Xbox 360", "PlayStation 3", "Wii", "Dreamcast", "PlayStation 2"]
   belongs_to :owner, class_name: 'User'
-
+  has_attachment :photo
   validates :name, presence: true
   validates :description, presence: true
   validates :console, inclusion: { in: CONSOLES }
