3cd15ed4a099ea937f5b619271d28be5c539e66f
James Dunn <james.ed.dunn@gmail.com>
2017-10-18 13:43:28 +0900

    rental datetime fix

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -13,9 +13,11 @@ class RentalsController < ApplicationController
     if @rental.save
       redirect_to rental_path(@rental)
     else
-      render game_path(@game)
+      flash[:alert] = "Please enter a valid rental date"
+      redirect_to game_path(@game)
     end
   end
+
 
   def show
     @rental = Rental.find(params[:id])
